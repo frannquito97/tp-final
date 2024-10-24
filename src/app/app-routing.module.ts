055d561075e869c51af6aed6509585ffd337aee3
Martin Miranda <tinchomirandda@gmail.com>
2024-10-24 20:20:23 -0300

    loginPage
    foto de fondo de pantalla loginPage, forms de login (WIP).

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,17 +6,19 @@ import { NotFoundComponent } from './page/not-found/not-found.component';
 import { HomeComponent } from './page/home/home.component';
 import { PlayGameComponent } from './page/play-game/play-game.component';
 import { UserViewComponent } from './page/user-view/user-view.component';
+import { LoginComponent } from './page/login/login.component';
 
 const routes: Routes = [
   { path:'home', component: HomeComponent},
   { path:'', redirectTo:'home', pathMatch: 'full' },
   { path:'play', component: PlayGameComponent },
   { path:'my-profile', component: UserViewComponent },
-  { path:'**' , component: NotFoundComponent },
+  { path:'f1Games', component: LoginComponent},
   /*{path:'modifyinfo', component: UserViewComponent},
   {path:'user/id:', component}
   {path:'user/id:', component}
   {path:'user/id:', component}*/
+  { path:'**' , component: NotFoundComponent }
 ];
 
 
